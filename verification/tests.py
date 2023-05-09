983735079424d640ae32165134f76c25a98167e9
retruc <97701409+retruc@users.noreply.github.com>
2023-05-09 14:58:13 +0200

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -9,18 +9,18 @@ Each test is dict with
 
 TESTS = {
     "Basics": [
-        {"input": 5.85987448205, "answer": 'e+pi', "explanation": ""},
-        {"input": 18.2958548951, "answer": 'e**e+pi', "explanation": ""},
-        {"input": 47.6085189284, "answer": 'e**e*pi', "explanation": ""},
-        {"input": -0.42331082513, "answer": 'e-pi', "explanation": ""},
-        {"input": 7.38905609893, "answer": 'e*e', "explanation": ""},
+        {"input": [5.85987448205], "answer": 'e+pi', "explanation": ""},
+        {"input": [18.2958548951], "answer": 'e**e+pi', "explanation": ""},
+        {"input": [47.6085189284], "answer": 'e**e*pi', "explanation": ""},
+        {"input": [-0.42331082513], "answer": 'e-pi', "explanation": ""},
+        {"input": [7.38905609893], "answer": 'e*e', "explanation": ""},
 
     ],
     "Extra": [
-        {"input": -2.48054830216, "answer": 'e*e-pi*pi', "explanation": ""},
-        {"input": 57.9087276547, "answer": 'e*pi**e-pi', "explanation": ""},
-        {"input": 0.07455076325, "answer": 'e/pi**pi', "explanation": ""},
-        {"input": 0.10132118364, "answer": 'e/e/pi/pi', "explanation": ""},
+        {"input": [-2.48054830216], "answer": 'e*e-pi*pi', "explanation": ""},
+        {"input": [57.9087276547], "answer": 'e*pi**e-pi', "explanation": ""},
+        {"input": [0.07455076325], "answer": 'e/pi**pi', "explanation": ""},
+        {"input": [0.10132118364], "answer": 'e/e/pi/pi', "explanation": ""},
 
     ]
 }
