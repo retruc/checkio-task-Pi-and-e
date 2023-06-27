ebfff3c2535efe72fa088ed22523061b7cc56604
Oleksandr Zozulia <freemanlex@gmail.com>
2023-06-27 21:52:31 +0300

    upd

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,4 +1,4 @@
-requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
+requirejs(['ext_editor_io2', 'jquery_190'],
     function (extIO, $) {
         var io = new extIO({});
         io.start();
